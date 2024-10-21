26f436622141ca0c2583927034a4d209f93a34d0
zyzzy123 <3041841261@qq.com>
2024-10-22 01:59:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2nLO6MZCiUcduLeeq8Z-_OnmiFs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'e8diie26yN-AmnACQZAlBhjjaudZuFl5VAdGkbyiu-o',
+      useTemplateId: '	jlw3JLRxSaxM3H6-kEk1w5jqRK0cYmEv8rEiq_klIGA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
